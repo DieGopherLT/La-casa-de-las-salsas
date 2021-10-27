ae94954f284f10fe3b6df1015d650f5f114b497a
Diego López Torres <buyeye09@gmail.com>
2021-10-26 19:12:22 -0500

    Menu page is now dynamic
    The sauces shown in menu page now are fetched from the database

--- models/Saucer.models.js
+++ models/Saucer.models.js
@@ -5,21 +5,26 @@ const Saucer = db.define('saucer', {
     code_S:{
         type: DataTypes.INTEGER({length:10}),
         primaryKey: true,
+        autoIncrement: true
     },
     name_S: {
         type: DataTypes.STRING(30),
         allowNull: false
     },
-    description_S: {
-        type: DataTypes.STRING(500),
-        allowNull: false
-    },
+    // description_S: {                     Will discuss this later
+    //     type: DataTypes.STRING(500),
+    //     allowNull: false
+    // },
     existence_S: {
         type: DataTypes.INTEGER({length:2}),
         allowNull: false,
     },
     price_S: {
         type: DataTypes.DECIMAL(4,2),
+        allowNull: false
+    },
+    image_S: {
+        type: DataTypes.STRING(100),
         allowNull: false
     }
 }, {
