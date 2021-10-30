b0a40b9f4edd761d5667e0f151ff3868dd115206
Alan Reyes <alan_2899@hotmail.com>
2021-10-30 17:59:17 -0500

    Userdate

---
+++ models/User.models.js
@@ -0,0 +1,40 @@
+const { DataTypes } = require('sequelize');
+const db = require('../config/db');
+
+const User = db.define('User', {
+    UserID: {
+        type: DataTypes.INTEGER({length:10}),
+        primaryKey: true,
+        autoIncrement: true,
+        unique: true
+    },
+    Name_User: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    Address_User: {
+        type: DataTypes.STRING(50),
+        allowNull: false
+    },
+    User_Phone: {
+        type: DataTypes.STRING(10),
+        allowNull: false
+    },
+    type_User: {
+        type: DataTypes.INTEGER({length:5}),
+        allowNull: false
+    },
+    Username: {
+        type: DataTypes.STRING(30),
+        allowNull: false,
+        unique: {
+            msg: "Ese usuario ya existe"
+        }
+    },
+    Password: {
+        type: DataTypes.STRING(60),
+        allowNull: false
+    }
+}, {
+    timestamps: false,
+});
