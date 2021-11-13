f651e0ad853cef418348146ec13b9fff68e19408
Alan Reyes <alan_2899@hotmail.com>
2021-11-13 17:58:43 -0600

    Me Quiero morir Doctor Garcia 3

--- config/server.js
+++ config/server.js
@@ -9,7 +9,8 @@ require('../models/User.models');
 require('../models/Saucer.models');
 require('../models/Sauces.models');
 require('../models/NewOrder.models');
-require('../models/Association');
+require('../models/받침접시-Order.models');
+require('../models/소스-Order.models');
 
 //Lo hago a modo de clase para que sea más legible.
 class Server {
